435ef005f0476ca6d58f9ee49b6c7eca3e35ba1e
tedwad <teddytarekegne@gmail.com>
2023-02-09 21:29:15 +0000

    add install directory

---
+++ install/lib/cmake/open62541/open62541Targets-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "open62541::open62541" for configuration "RelWithDebInfo"
+set_property(TARGET open62541::open62541 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(open62541::open62541 PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libopen62541.so.1.3.4"
+  IMPORTED_SONAME_RELWITHDEBINFO "libopen62541.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS open62541::open62541 )
+list(APPEND _IMPORT_CHECK_FILES_FOR_open62541::open62541 "${_IMPORT_PREFIX}/lib/libopen62541.so.1.3.4" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
