de934ab8ea2ac18422e9e109121333580778bea5
tedwad <89061708+tedwad@users.noreply.github.com>
2023-02-09 11:20:09 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+
+#include "myServer.h"
+
+#include <signal.h>
+#include <stdlib.h>
+
+static volatile UA_Boolean running = true;
+
+static void stopHandler(int sig) {
+	UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+	running = false;
+}
+//	Main Server
+
+int main(int argc, char** argv) {
+	signal(SIGINT, stopHandler);
+	signal(SIGTERM,stopHandler);
+
+	UA_Server *server = UA_Server_new();
+	UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+
+	UA_StatusCode retval;
+	
+	if(myServer(server) != UA_STATUSCODE_GOOD) {
+        	UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Could not add the nodeset. "
+        	"Check previous output for any error.");
+        	retval = UA_STATUSCODE_BADUNEXPECTEDERROR;
+	}
+	else {
+		retval = UA_Server_run(server, &running);
+	}
+	UA_Server_delete(server);
+	return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+}
